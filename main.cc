33d072f53a8056548b785b1a7e333e5dd0e8dbd5
Brian <brian@Brians-MacBook-Air.local>
2024-02-26 11:44:42 -0500

    Parameterize benchmarks with probabilites element appearance in the set.

--- main.cc
+++ main.cc
@@ -1,4 +1,3 @@
-
 #include <benchmark/benchmark.h>
 
 #include <bitset>
@@ -39,11 +38,11 @@ absl::StatusOr<std::vector<uint32_t>> GetSetMembers(uint32_t start_member,
 
 absl::StatusOr<std::pair<std::vector<uint32_t>, std::vector<uint32_t>>>
 GetPairOfSetMembers(double probability) {
-    auto left = GetSetMembers(0, kNumSetElems, 1);
+    auto left = GetSetMembers(0, kNumSetElems, probability);
     if (!left.ok()) {
         return left.status();
     }
-    auto right = GetSetMembers(kNumSetElems / 2, kNumSetElems / 2, 1);
+    auto right = GetSetMembers(kNumSetElems / 2, kNumSetElems / 2, probability);
     if (!right.ok()) {
         return right.status();
     }
@@ -55,8 +54,8 @@ GetPairOfSetMembers(double probability) {
 absl::StatusOr<
     std::pair<std::unordered_set<std::string>, std::unordered_set<std::string>>>
 GetUnorderedStringSetsHelper(
-    absl::AnyInvocable<std::string(uint32_t)> converter) {
-    auto sets = GetPairOfSetMembers(1);
+    absl::AnyInvocable<std::string(uint32_t)> converter, double probability) {
+    auto sets = GetPairOfSetMembers(probability);
     if (!sets.ok()) {
         return sets.status();
     }
@@ -72,45 +71,9 @@ GetUnorderedStringSetsHelper(
     return std::make_pair(std::move(left_set), std::move(right_set));
 }
 
-absl::StatusOr<std::pair<std::bitset<kNumSetElems>, std::bitset<kNumSetElems>>>
-GetUnorderedBitSets() {
-    auto sets = GetPairOfSetMembers(1);
-    if (!sets.ok()) {
-        return sets.status();
-    }
-    auto [left, right] = std::move(*sets);
-    std::bitset<kNumSetElems> left_set;
-    for (auto v : left) {
-        left_set.set(v);
-    }
-    std::bitset<kNumSetElems> right_set;
-    for (auto v : right) {
-        right_set.set(v);
-    }
-    return std::make_pair(std::move(left_set), std::move(right_set));
-}
-
-absl::StatusOr<std::pair<roaring::Roaring, roaring::Roaring>> GetRoaringSets() {
-    auto sets = GetPairOfSetMembers(1);
-    if (!sets.ok()) {
-        return sets.status();
-    }
-    auto [left, right] = std::move(*sets);
-    roaring::Roaring left_set;
-    for (auto v : left) {
-        left_set.add(v);
-    }
-    roaring::Roaring right_set;
-    for (auto v : right) {
-        right_set.add(v);
-    }
-    left_set.runOptimize();
-    right_set.runOptimize();
-    return std::make_pair(std::move(left_set), std::move(right_set));
-}
-
 template <typename T, typename U>
 void BenchmarkSetHelper(benchmark::State& state, T set_getter, U op) {
+    set_getter.SetProbabilityPercent(state.range(0));
     auto sets = set_getter();
     ASSERT_TRUE(sets.ok());
     for (auto _ : state) {
@@ -122,12 +85,24 @@ void BenchmarkSetHelper(benchmark::State& state, T set_getter, U op) {
     }
 }
 
-class UnorderedIntSet {
+class BaseGetter {
+   public:
+    void SetProbabilityPercent(uint8_t probability_percent) {
+        probability_ = probability_percent / 100.0;
+    }
+
+    double GetProbability() const { return probability_; }
+
+   private:
+    double probability_ = 1;
+};
+
+class UnorderedIntSet : public BaseGetter {
    public:
     absl::StatusOr<
         std::pair<std::unordered_set<uint32_t>, std::unordered_set<uint32_t>>>
     operator()() {
-        auto sets = GetPairOfSetMembers(1);
+        auto sets = GetPairOfSetMembers(GetProbability());
         if (!sets.ok()) {
             return sets.status();
         }
@@ -140,36 +115,38 @@ class UnorderedIntSet {
 
 // The bytes of an integer representation of the value in a string.
 // The length of the byte array is the sizeof each set member.
-class UnorderedByteSet {
-   public:
-    absl::StatusOr<std::pair<std::unordered_set<std::string>,
-                             std::unordered_set<std::string>>>
-    operator()() {
-        return GetUnorderedStringSetsHelper([](uint32_t val) {
-            return std::string(reinterpret_cast<const char*>(&val),
-                               sizeof(val));
-        });
-    }
-};
-
-// The integer represented as a string.
-// Demonstrates the implications of a longer string value.
-class UnorderedStringSet {
+class UnorderedByteSet : public BaseGetter {
    public:
     absl::StatusOr<std::pair<std::unordered_set<std::string>,
                              std::unordered_set<std::string>>>
     operator()() {
         return GetUnorderedStringSetsHelper(
-            [](uint32_t val) { return std::to_string(val); });
+            [](uint32_t val) {
+                return std::string(reinterpret_cast<const char*>(&val),
+                                   sizeof(val));
+            },
+            GetProbability());
     }
 };
 
-class BitSet {
+// The integer represented as a string.
+// Demonstrates the implications of a longer string value.
+class UnorderedStringSet : public BaseGetter {
+   public:
+    absl::StatusOr<std::pair<std::unordered_set<std::string>,
+                             std::unordered_set<std::string>>>
+    operator()() {
+        return GetUnorderedStringSetsHelper(
+            [](uint32_t val) { return std::to_string(val); }, GetProbability());
+    }
+};
+
+class BitSet : public BaseGetter {
    public:
     absl::StatusOr<
         std::pair<std::bitset<kNumSetElems>, std::bitset<kNumSetElems>>>
     operator()() {
-        auto sets = GetPairOfSetMembers(1);
+        auto sets = GetPairOfSetMembers(GetProbability());
         if (!sets.ok()) {
             return sets.status();
         }
@@ -186,10 +163,10 @@ class BitSet {
     }
 };
 
-class RoaringBitSet {
+class RoaringBitSet : public BaseGetter {
    public:
     absl::StatusOr<std::pair<roaring::Roaring, roaring::Roaring>> operator()() {
-        auto sets = GetPairOfSetMembers(1);
+        auto sets = GetPairOfSetMembers(GetProbability());
         if (!sets.ok()) {
             return sets.status();
         }
@@ -280,75 +257,104 @@ class RoaringBitwiseDifferenceOp {
 
 template <typename T, typename U>
 class SetOpFixture : public benchmark::Fixture {
+   public:
+    void SetUp(::benchmark::State& state) {}
+
    protected:
     T getter_;
     U op_;
 };
 
 // Unordered set of integers
-BENCHMARK_TEMPLATE_F(SetOpFixture, IntTestUnion, UnorderedIntSet,
-                     UnionOp<UnorderedIntSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, IntTestUnion, UnorderedIntSet,
+                            UnionOp<UnorderedIntSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, IntTestUnion)->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, IntTestIntersection, UnorderedIntSet,
-                     IntersectionOp<UnorderedIntSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, IntTestIntersection, UnorderedIntSet,
+                            IntersectionOp<UnorderedIntSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, IntTestIntersection)->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, IntTestDifference, UnorderedIntSet,
-                     DifferenceOp<UnorderedIntSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, IntTestDifference, UnorderedIntSet,
+                            DifferenceOp<UnorderedIntSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, IntTestDifference)->DenseRange(0, 100, 25);
 
 // Unordered set of bytes
-BENCHMARK_TEMPLATE_F(SetOpFixture, ByteTestUnion, UnorderedByteSet,
-                     UnionOp<UnorderedByteSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, ByteTestUnion, UnorderedByteSet,
+                            UnionOp<UnorderedByteSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, ByteTestUnion)->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, ByteTestIntersection, UnorderedByteSet,
-                     IntersectionOp<UnorderedByteSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, ByteTestIntersection,
+                            UnorderedByteSet, IntersectionOp<UnorderedByteSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, ByteTestIntersection)
+    ->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, ByteTestDifference, UnorderedByteSet,
-                     DifferenceOp<UnorderedByteSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, ByteTestDifference, UnorderedByteSet,
+                            DifferenceOp<UnorderedByteSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, ByteTestDifference)->DenseRange(0, 100, 25);
 
 // Unordered set of strings
-BENCHMARK_TEMPLATE_F(SetOpFixture, StringTestUnion, UnorderedStringSet,
-                     UnionOp<UnorderedStringSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, StringTestUnion, UnorderedStringSet,
+                            UnionOp<UnorderedStringSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, StringTestUnion)->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, StringTestIntersection, UnorderedStringSet,
-                     IntersectionOp<UnorderedStringSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, StringTestIntersection,
+                            UnorderedStringSet,
+                            IntersectionOp<UnorderedStringSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, StringTestIntersection)
+    ->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, StringTestDifference, UnorderedStringSet,
-                     DifferenceOp<UnorderedStringSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, StringTestDifference,
+                            UnorderedStringSet,
+                            DifferenceOp<UnorderedStringSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, StringTestDifference)
+    ->DenseRange(0, 100, 25);
 
 // std::bitset implementation
-BENCHMARK_TEMPLATE_F(SetOpFixture, StdBitSetTestUnion, BitSet,
-                     BitwiseUnionOp<BitSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, StdBitSetTestUnion, BitSet,
+                            BitwiseUnionOp<BitSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, StdBitSetTestUnion)->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, StdBitSetTestIntersection, BitSet,
-                     BitwiseIntersectionOp<BitSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, StdBitSetTestIntersection, BitSet,
+                            BitwiseIntersectionOp<BitSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, StdBitSetTestIntersection)
+    ->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, StdBitSetTestDifference, BitSet,
-                     BitwiseDifferenceOp<BitSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, StdBitSetTestDifference, BitSet,
+                            BitwiseDifferenceOp<BitSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, StdBitSetTestDifference)
+    ->DenseRange(0, 100, 25);
 
 // Roaring bitset implementation
-BENCHMARK_TEMPLATE_F(SetOpFixture, RoaringBitSetTestUnion, RoaringBitSet,
-                     BitwiseUnionOp<RoaringBitSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, RoaringBitSetTestUnion, RoaringBitSet,
+                            BitwiseUnionOp<RoaringBitSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, RoaringBitSetTestUnion)
+    ->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, RoaringBitSetTestIntersection, RoaringBitSet,
-                     BitwiseIntersectionOp<RoaringBitSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, RoaringBitSetTestIntersection,
+                            RoaringBitSet, BitwiseIntersectionOp<RoaringBitSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, RoaringBitSetTestIntersection)
+    ->DenseRange(0, 100, 25);
 
-BENCHMARK_TEMPLATE_F(SetOpFixture, RoaringBitSetTestDifference, RoaringBitSet,
-                     RoaringBitwiseDifferenceOp<RoaringBitSet>)
+BENCHMARK_TEMPLATE_DEFINE_F(SetOpFixture, RoaringBitSetTestDifference,
+                            RoaringBitSet,
+                            RoaringBitwiseDifferenceOp<RoaringBitSet>)
 (benchmark::State& state) { BenchmarkSetHelper(state, getter_, op_); }
+BENCHMARK_REGISTER_F(SetOpFixture, RoaringBitSetTestDifference)
+    ->DenseRange(0, 100, 25);
 
 // Run the benchmark
 BENCHMARK_MAIN();
